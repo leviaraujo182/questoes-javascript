c5bed5ca201c5d5f681d6add32fce86973b54f90
leviaraujo182 <31797494+leviaraujo182@users.noreply.github.com>
2021-01-22 08:33:49 -0300

    Questao 7

---
+++ Questao7.js
@@ -0,0 +1,15 @@
+function maximominimo(numero, minimo, maximo) {
+    var inclusivo = false;
+    if (numero >= minimo && numero <= maximo) {
+        inclusivo = true;
+    } else {
+        inclusivo = false;
+    }
+    if (inclusivo == true) {
+        console.log("Entre")
+    } else {
+
+    }
+}
+
+maximominimo(15, 10, 20);
